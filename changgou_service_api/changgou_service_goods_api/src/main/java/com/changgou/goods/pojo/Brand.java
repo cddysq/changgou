736ba9d780a51db290b781b0727fdd19f054bbf8
Rainbow Cat <2056423011@qq.com>
2020-01-24 14:47:23 +0800

    畅购商城品牌服务实体

---
+++ changgou_service_api/changgou_service_goods_api/src/main/java/com/changgou/goods/pojo/Brand.java
@@ -0,0 +1,49 @@
+package com.changgou.goods.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+/**
+ * @Author: Haotian
+ * @Date: 2020/1/24 14:02
+ * @Description: 品牌实体类
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "tb_brand")
+public class Brand implements Serializable {
+    private static final long serialVersionUID = -3606289835684702086L;
+    /**
+     * 品牌id
+     */
+    @Id
+    private Integer id;
+
+    /**
+     * 品牌名称
+     */
+    private String name;
+
+    /**
+     * 品牌图片地址
+     */
+    private String image;
+
+    /**
+     * 品牌的首字母
+     */
+    private String letter;
+
+    /**
+     * 排序
+     */
+    private Integer seq;
+}
