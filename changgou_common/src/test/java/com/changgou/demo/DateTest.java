175a3fc0a3833a77aa85773a75e9f162f429b10d
Rainbow Cat <2056423011@qq.com>
2020-02-13 19:56:35 +0800

    畅购商城时间差测试

---
+++ changgou_common/src/test/java/com/changgou/demo/DateTest.java
@@ -0,0 +1,17 @@
+package com.changgou.demo;
+
+import cn.hutool.core.date.DateUtil;
+import org.junit.Test;
+
+/**
+ * @Author: Haotian
+ * @Date: 2020/2/13 19:39
+ * @Description: 计算两个时相差天数
+ */
+public class DateTest {
+    @Test
+    public void timeDifference() {
+        long day = DateUtil.betweenDay( DateUtil.date(), DateUtil.parseDate( "2020-02-10" ), true );
+        System.out.println( "相差" + day + "天" );
+    }
+}
