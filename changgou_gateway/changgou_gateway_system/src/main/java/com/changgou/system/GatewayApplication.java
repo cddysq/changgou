31fe9a727feaacf4aab02dd95e7c058f54e854df
Rainbow Cat <2056423011@qq.com>
2020-02-14 18:21:15 +0800

    畅购商城后台网关限流

--- changgou_gateway/changgou_gateway_system/src/main/java/com/changgou/system/GatewayApplication.java
+++ changgou_gateway/changgou_gateway_system/src/main/java/com/changgou/system/GatewayApplication.java
@@ -2,7 +2,13 @@ package com.changgou.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
 
 /**
  * @Author: Haotian
@@ -14,5 +20,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run( GatewayApplication.class );
+    }
+
+    @Bean
+    public KeyResolver ipKeyResolver() {
+        return exchange -> Mono.just( Objects.requireNonNull( exchange.getRequest().getRemoteAddress() ).getHostName() );
     }
 }
