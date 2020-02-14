c762517f764542817488437dc6b038a8422b9fe5
Rainbow Cat <2056423011@qq.com>
2020-02-14 21:39:35 +0800

    畅购商城后台JWT认证

---
+++ changgou_gateway/changgou_gateway_system/src/main/java/com/changgou/system/filter/AuthorizeFilter.java
@@ -0,0 +1,26 @@
+package com.changgou.system.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @Author: Haotian
+ * @Date: 2020/2/14 18:04
+ * @Description: 权限校验
+ */
+@Component
+public class AuthorizeFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return chain.filter( exchange );
+    }
+
+    @Override
+    public int getOrder() {
+        return 2;
+    }
+}
