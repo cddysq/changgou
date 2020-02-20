18b56eae5e24790c3133fd08d25caa50edcaa147
Rainbow Cat <2056423011@qq.com>
2020-02-20 16:21:18 +0800

    使用api进制转换

--- changgou_common/src/test/java/com/changgou/demo/DecimalToBinaryTest.java
+++ changgou_common/src/test/java/com/changgou/demo/DecimalToBinaryTest.java
@@ -19,4 +19,18 @@ public class DecimalToBinaryTest {
         String convert2 = DecimalToBinary.convert( y );
         Assert.assertEquals( "0", convert2 );
     }
+
+    @Test
+    public void conversionOfNumberSystems() {
+        int i = 10;
+        //转为二进制
+        Assert.assertEquals( "1010", Integer.toBinaryString( i ) );
+        //转为八进制
+        System.out.println( i + " 的八进制为：" + Integer.toOctalString( i ) );
+        //转为十六进制
+        System.out.println( i + " 的十六进制为：" + Integer.toHexString( i ) );
+        //转为三进制
+        System.out.println( i + " 的三进制为：" + Integer.toString( i, 3 ) );
+
+    }
 }
