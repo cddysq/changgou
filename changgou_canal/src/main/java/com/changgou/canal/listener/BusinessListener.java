ab2cd48655c05ae161bfe4876f5e6bb4489d285f
Rainbow Cat <2056423011@qq.com>
2020-02-20 17:03:27 +0800

    广告位监听优化

--- changgou_canal/src/main/java/com/changgou/canal/listener/BusinessListener.java
+++ changgou_canal/src/main/java/com/changgou/canal/listener/BusinessListener.java
@@ -8,6 +8,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Author: Haotian
  * @Date: 2020/2/18 19:35
@@ -16,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 @CanalEventListener //声明当前的类是canal的监听类
 @Slf4j
 public class BusinessListener {
+    private static final String AD_POSITION = "position";
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
@@ -28,14 +32,25 @@ public class BusinessListener {
     @ListenPoint(schema = "changgou_business", table = "tb_ad")
     public void adUpdate(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
         log.info( "广告表数据发生改变" );
-        //TODO: 2020/2/18 20:31 广告位发生改变，删除原有缓存数据
+        //遍历改变之前的数据
+        Map<Object, Object> oldData = new HashMap<>( 0 );
+        rowData.getBeforeColumnsList().forEach( c -> oldData.put( c.getName(), c.getValue() ) );
+
+        //遍历改变之后的数据
+        Map<Object, Object> newData = new HashMap<>( 0 );
         for (CanalEntry.Column column : rowData.getAfterColumnsList()) {
-            if ("position".equals( column.getName() )) {
+            newData.put( column.getName(), column.getValue() );
+            if (AD_POSITION.equals( column.getName() )) {
                 log.info( "发送最新的数据到MQ：{}", column.getValue() );
                 //发送消息
                 rabbitTemplate.convertAndSend( "", RabbitMQConfig.AD_UPDATE_QUEUE, column.getValue() );
             }
         }
-
+        //广告位置发生改变，需额外更新就广告位数据
+        if (!newData.get( AD_POSITION ).equals( oldData.get( AD_POSITION ) )) {
+            log.info( "广告位发生改变，发送旧广告位数据到MQ：{}", oldData.get( AD_POSITION ) );
+            //发送消息
+            rabbitTemplate.convertAndSend( "", RabbitMQConfig.AD_UPDATE_QUEUE, oldData.get( AD_POSITION ) );
+        }
     }
 }
