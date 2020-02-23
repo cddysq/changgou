7f33fd99475ca41d15d155b26346ee4edd8a75de
Rainbow Cat <2056423011@qq.com>
2020-02-24 00:25:14 +0800

    发送请求测试

--- changgou-user-oauth/src/main/java/com/changgou/OAuthApplication.java
+++ changgou-user-oauth/src/main/java/com/changgou/OAuthApplication.java
@@ -3,6 +3,8 @@ package com.changgou;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
@@ -19,4 +21,8 @@ public class OAuthApplication {
         SpringApplication.run( OAuthApplication.class, args );
     }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
