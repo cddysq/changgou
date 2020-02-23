99a8171b1f61a54800aa64b694350a011cdbea6e
Rainbow Cat <2056423011@qq.com>
2020-02-23 23:00:27 +0800

    update

--- changgou-user-oauth/src/test/java/com/changgou/oauth/ParseJwtTest.java
+++ changgou-user-oauth/src/test/java/com/changgou/oauth/ParseJwtTest.java
@@ -15,6 +15,9 @@ public class ParseJwtTest {
      * jwt
      */
     private static final String jwt = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoi6Im-6I6J5LidIiwiYWdlIjoiMTgifQ.IBLmnYOFv_i45-iXL0IPIunjcPbBRFVh0JWE68ZpsdMajbpnisUVxC77fIxMSZmDYrienE7mehloY_QYwvLHg-NwjgXWeCQyBDSZpQpxu5-pFNG-TVmjDfr23cDL3Rd3YPz_Azktu6nyRAwjr1S5FL7DVPyPN6JQEWVXvt1qO5H5lhAqnehUc8LRAiARf2clxZubPh6yjsW0ziHGZYF4MzOJZ8mTd8LZjodO3yPqOkTLBVzumMdmLTwMUjDKOR98ytqS0wtG-51cckZ6IXMf1jrtLWYJ_I8d9V4jFvne9ciCPBUOInDRgoc1AA2NInhF6M2W25Q5JvhSLBwNvFGBZA";
+    /**
+     * 公钥
+     */
     private static final String publicKey = "-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvFsEiaLvij9C1Mz+oyAmt47whAaRkRu/8kePM+X8760UGU0RMwGti6Z9y3LQ0RvK6I0brXmbGB/RsN38PVnhcP8ZfxGUH26kX0RK+tlrxcrG+HkPYOH4XPAL8Q1lu1n9x3tLcIPxq8ZZtuIyKYEmoLKyMsvTviG5flTpDprT25unWgE4md1kthRWXOnfWHATVY7Y/r4obiOL1mS5bEa/iNKotQNnvIAKtjBM4RlIDWMa6dmz+lHtLtqDD2LF1qwoiSIHI75LQZ/CNYaHCfZSxtOydpNKq8eb1/PGiLNolD4La2zf0/1dlcr5mkesV570NxRmU1tFm8Zd3MZlZmyv9QIDAQAB-----END PUBLIC KEY-----";
 
     @Test
