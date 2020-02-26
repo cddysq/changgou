0c2d5ee1aea0397d6caf3b5b673ab0ebd2f606d6
Rainbow Cat <2056423011@qq.com>
2020-02-26 18:57:26 +0800

    创建订单工程

---
+++ changgou_service/changgou_service_order/src/main/java/com/changgou/order/OrderApplication.java
@@ -0,0 +1,23 @@
+package com.changgou.order;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import tk.mybatis.spring.annotation.MapperScan;
+
+/**
+ * @Author: Haotian
+ * @Date: 2020/2/26 18:54
+ * @Description: 启动类
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+@MapperScan(basePackages = {"com.changgou.order.dao"})
+@EnableFeignClients(basePackages = {"com.changgou.goods.feign"})
+public class OrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run( OrderApplication.class );
+    }
+}
