310a785bf71ca9d56c024a3a624ba3a7e157b169
Rainbow Cat <2056423011@qq.com>
2020-02-26 10:39:10 +0800

    update

--- changgou_service/changgou_service_goods/src/main/java/com/changgou/service/goods/service/impl/CategoryServiceImpl.java
+++ changgou_service/changgou_service_goods/src/main/java/com/changgou/service/goods/service/impl/CategoryServiceImpl.java
@@ -1,5 +1,7 @@
 package com.changgou.service.goods.service.impl;
 
+import cn.hutool.core.convert.Convert;
+import cn.hutool.core.util.StrUtil;
 import com.changgou.goods.pojo.Category;
 import com.changgou.service.goods.dao.CategoryMapper;
 import com.changgou.service.goods.service.CategoryService;
@@ -74,39 +76,45 @@ public class CategoryServiceImpl implements CategoryService {
         Example example = new Example( Category.class );
         Example.Criteria criteria = example.createCriteria();
         if (searchMap != null) {
-            //TODO: 2020/2/18 21:46  代码优化
-
             // 分类名称
-            if (searchMap.get( "name" ) != null && !"".equals( searchMap.get( "name" ) )) {
-                criteria.andLike( "name", "%" + searchMap.get( "name" ) + "%" );
+            String name = Convert.toStr( searchMap.get( "name" ) );
+            if (StrUtil.isNotEmpty( name )) {
+                criteria.andLike( "name", "%" + name + "%" );
             }
             // 是否显示
-            if (searchMap.get( "isShow" ) != null && !"".equals( searchMap.get( "isShow" ) )) {
-                criteria.andEqualTo( "isShow", searchMap.get( "isShow" ) );
+            String isShow = Convert.toStr( searchMap.get( "isShow" ) );
+            if (StrUtil.isNotEmpty( isShow )) {
+                criteria.andEqualTo( "isShow", isShow );
             }
             // 是否导航
-            if (searchMap.get( "isMenu" ) != null && !"".equals( searchMap.get( "isMenu" ) )) {
-                criteria.andLike( "isMenu", "%" + searchMap.get( "isMenu" ) + "%" );
+            String isMenu = Convert.toStr( searchMap.get( "isMenu" ) );
+            if (StrUtil.isNotEmpty( isMenu )) {
+                criteria.andLike( "isMenu", "%" + isMenu + "%" );
             }
             // 分类ID
-            if (searchMap.get( "id" ) != null) {
-                criteria.andEqualTo( "id", searchMap.get( "id" ) );
+            String id = Convert.toStr( searchMap.get( "id" ) );
+            if (StrUtil.isNotEmpty( id )) {
+                criteria.andEqualTo( "id", id );
             }
             // 商品数量
-            if (searchMap.get( "goodsNum" ) != null) {
-                criteria.andEqualTo( "goodsNum", searchMap.get( "goodsNum" ) );
+            String goodsNum = Convert.toStr( searchMap.get( "goodsNum" ) );
+            if (StrUtil.isNotEmpty( goodsNum )) {
+                criteria.andEqualTo( "goodsNum", goodsNum );
             }
             // 排序
-            if (searchMap.get( "seq" ) != null) {
-                criteria.andEqualTo( "seq", searchMap.get( "seq" ) );
+            String seq = Convert.toStr( searchMap.get( "seq" ) );
+            if (StrUtil.isNotEmpty( seq )) {
+                criteria.andEqualTo( "seq", seq );
             }
             // 上级ID
-            if (searchMap.get( "parentId" ) != null) {
-                criteria.andEqualTo( "parentId", searchMap.get( "parentId" ) );
+            String parentId = Convert.toStr( searchMap.get( "parentId" ) );
+            if (StrUtil.isNotEmpty( parentId )) {
+                criteria.andEqualTo( "parentId", parentId );
             }
             // 模板ID
-            if (searchMap.get( "templateId" ) != null) {
-                criteria.andEqualTo( "templateId", searchMap.get( "templateId" ) );
+            String templateId = Convert.toStr( searchMap.get( "templateId" ) );
+            if (StrUtil.isNotEmpty( templateId )) {
+                criteria.andEqualTo( "templateId", templateId );
             }
         }
         return example;
--- changgou_service/changgou_service_user/src/main/java/com/changgou/user/service/impl/UserServiceImpl.java
+++ changgou_service/changgou_service_user/src/main/java/com/changgou/user/service/impl/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.changgou.user.service.impl;
 
+import cn.hutool.core.convert.Convert;
+import cn.hutool.core.util.StrUtil;
 import com.changgou.user.dao.UserMapper;
 import com.changgou.user.pojo.User;
 import com.changgou.user.service.UserService;
@@ -74,71 +76,85 @@ public class UserServiceImpl implements UserService {
         Example example = new Example( User.class );
         Example.Criteria criteria = example.createCriteria();
         if (searchMap != null) {
-            //TODO: 2020/2/23 23:18 字段优化
-
             // 用户名
-            if (searchMap.get( "username" ) != null && !"".equals( searchMap.get( "username" ) )) {
-                criteria.andEqualTo( "username", searchMap.get( "username" ) );
+            String username = Convert.toStr( searchMap.get( "username" ) );
+            if (StrUtil.isNotEmpty( username )) {
+                criteria.andEqualTo( "username", username );
             }
             // 密码，加密存储
-            if (searchMap.get( "password" ) != null && !"".equals( searchMap.get( "password" ) )) {
-                criteria.andEqualTo( "password", searchMap.get( "password" ) );
+            String password = Convert.toStr( searchMap.get( "password" ) );
+            if (StrUtil.isNotEmpty( password )) {
+                criteria.andEqualTo( "password", password );
             }
             // 注册手机号
-            if (searchMap.get( "phone" ) != null && !"".equals( searchMap.get( "phone" ) )) {
-                criteria.andLike( "phone", "%" + searchMap.get( "phone" ) + "%" );
+            String phone = Convert.toStr( searchMap.get( "phone" ) );
+            if (StrUtil.isNotEmpty( phone )) {
+                criteria.andLike( "phone", "%" + phone + "%" );
             }
             // 注册邮箱
-            if (searchMap.get( "email" ) != null && !"".equals( searchMap.get( "email" ) )) {
-                criteria.andLike( "email", "%" + searchMap.get( "email" ) + "%" );
+            String email = Convert.toStr( searchMap.get( "email" ) );
+            if (StrUtil.isNotEmpty( email )) {
+                criteria.andLike( "email", "%" + email + "%" );
             }
             // 会员来源：1:PC，2：H5，3：Android，4：IOS
-            if (searchMap.get( "sourceType" ) != null && !"".equals( searchMap.get( "sourceType" ) )) {
-                criteria.andEqualTo( "sourceType", searchMap.get( "sourceType" ) );
+            String sourceType = Convert.toStr( searchMap.get( "sourceType" ) );
+            if (StrUtil.isNotEmpty( sourceType )) {
+                criteria.andEqualTo( "sourceType", sourceType );
             }
             // 昵称
-            if (searchMap.get( "nickName" ) != null && !"".equals( searchMap.get( "nickName" ) )) {
-                criteria.andLike( "nickName", "%" + searchMap.get( "nickName" ) + "%" );
+            String nickName = Convert.toStr( searchMap.get( "nickName" ) );
+            if (StrUtil.isNotEmpty( nickName )) {
+                criteria.andLike( "nickName", "%" + nickName + "%" );
             }
             // 真实姓名
-            if (searchMap.get( "name" ) != null && !"".equals( searchMap.get( "name" ) )) {
-                criteria.andLike( "name", "%" + searchMap.get( "name" ) + "%" );
+            String name = Convert.toStr( searchMap.get( "name" ) );
+            if (StrUtil.isNotEmpty( name )) {
+                criteria.andLike( "name", "%" + name + "%" );
             }
             // 使用状态（1正常 0非正常）
-            if (searchMap.get( "status" ) != null && !"".equals( searchMap.get( "status" ) )) {
-                criteria.andEqualTo( "status", searchMap.get( "status" ) );
+            String status = Convert.toStr( searchMap.get( "status" ) );
+            if (StrUtil.isNotEmpty( status )) {
+                criteria.andEqualTo( "status", status );
             }
             // 头像地址
-            if (searchMap.get( "headPic" ) != null && !"".equals( searchMap.get( "headPic" ) )) {
-                criteria.andLike( "headPic", "%" + searchMap.get( "headPic" ) + "%" );
+            String headPic = Convert.toStr( searchMap.get( "headPic" ) );
+            if (StrUtil.isNotEmpty( headPic )) {
+                criteria.andLike( "headPic", "%" + headPic + "%" );
             }
             // QQ号码
-            if (searchMap.get( "qq" ) != null && !"".equals( searchMap.get( "qq" ) )) {
-                criteria.andLike( "qq", "%" + searchMap.get( "qq" ) + "%" );
+            String qq = Convert.toStr( searchMap.get( "qq" ) );
+            if (StrUtil.isNotEmpty( qq )) {
+                criteria.andLike( "qq", "%" + qq + "%" );
             }
             // 手机是否验证 （0否  1是）
-            if (searchMap.get( "isMobileCheck" ) != null && !"".equals( searchMap.get( "isMobileCheck" ) )) {
-                criteria.andEqualTo( "isMobileCheck", searchMap.get( "isMobileCheck" ) );
+            String isMobileCheck = Convert.toStr( searchMap.get( "isMobileCheck" ) );
+            if (StrUtil.isNotEmpty( isMobileCheck )) {
+                criteria.andEqualTo( "isMobileCheck", isMobileCheck );
             }
             // 邮箱是否检测（0否  1是）
-            if (searchMap.get( "isEmailCheck" ) != null && !"".equals( searchMap.get( "isEmailCheck" ) )) {
-                criteria.andEqualTo( "isEmailCheck", searchMap.get( "isEmailCheck" ) );
+            String isEmailCheck = Convert.toStr( searchMap.get( "isEmailCheck" ) );
+            if (StrUtil.isNotEmpty( isEmailCheck )) {
+                criteria.andEqualTo( "isEmailCheck", isEmailCheck );
             }
             // 性别，1男，0女
-            if (searchMap.get( "sex" ) != null && !"".equals( searchMap.get( "sex" ) )) {
-                criteria.andEqualTo( "sex", searchMap.get( "sex" ) );
+            String sex = Convert.toStr( searchMap.get( "sex" ) );
+            if (StrUtil.isNotEmpty( sex )) {
+                criteria.andEqualTo( "sex", sex );
             }
             // 会员等级
-            if (searchMap.get( "userLevel" ) != null) {
-                criteria.andEqualTo( "userLevel", searchMap.get( "userLevel" ) );
+            String userLevel = Convert.toStr( searchMap.get( "userLevel" ) );
+            if (StrUtil.isNotEmpty( userLevel )) {
+                criteria.andEqualTo( "userLevel", userLevel );
             }
             // 积分
-            if (searchMap.get( "points" ) != null) {
-                criteria.andEqualTo( "points", searchMap.get( "points" ) );
+            String points = Convert.toStr( searchMap.get( "points" ) );
+            if (StrUtil.isNotEmpty( points )) {
+                criteria.andEqualTo( "points", points );
             }
             // 经验值
-            if (searchMap.get( "experienceValue" ) != null) {
-                criteria.andEqualTo( "experienceValue", searchMap.get( "experienceValue" ) );
+            String experienceValue = Convert.toStr( searchMap.get( "experienceValue" ) );
+            if (StrUtil.isNotEmpty( experienceValue )) {
+                criteria.andEqualTo( "experienceValue", experienceValue );
             }
         }
         return example;
