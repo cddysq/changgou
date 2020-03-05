7d7601ab1d1da7350e78019f50ec8fbd1a2a8c80
Rainbow Cat <2056423011@qq.com>
2020-03-05 21:33:06 +0800

    秒杀下单

---
+++ changgou_web/changgou_web_seckill/src/main/java/com/changgou/seckill/web/controller/SecKillOrderController.java
@@ -0,0 +1,28 @@
+package com.changgou.seckill.web.controller;
+
+import com.changgou.common.pojo.Result;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: Haotian
+ * @Date: 2020/3/5 21:30
+ * @Description: 抢单接口
+ */
+@RestController
+@RequestMapping("/wseckillorder")
+public class SecKillOrderController {
+    /**
+     * 抢单
+     *
+     * @param time 当前时间段
+     * @param id   商品id
+     * @return 抢单信息
+     */
+    @GetMapping("/add")
+    public Result<Object> add(@RequestParam("time") String time, @RequestParam("id") Long id) {
+        return null;
+    }
+}
