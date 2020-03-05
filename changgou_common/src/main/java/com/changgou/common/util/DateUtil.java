7461e9c2d8411cdce007cf9e0b5edb27d4821a1a
Rainbow Cat <2056423011@qq.com>
2020-03-05 19:04:10 +0800

    秒杀商品加入缓存

---
+++ changgou_common/src/main/java/com/changgou/common/util/DateUtil.java
@@ -0,0 +1,103 @@
+package com.changgou.common.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+public class DateUtil {
+
+    /**
+     * 从yyyy-MM-dd HH:mm格式转成yyyyMMddHH格式
+     */
+    public static String formatStr(String dateStr) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat( "yyyy-MM-dd HH:mm" );
+        try {
+            Date date = simpleDateFormat.parse( dateStr );
+            simpleDateFormat = new SimpleDateFormat( "yyyyMMddHH" );
+            return simpleDateFormat.format( date );
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 获取指定日期的凌晨
+     */
+    public static Date toDayStartHour(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime( date );
+        calendar.set( Calendar.HOUR_OF_DAY, 0 );
+        calendar.set( Calendar.MINUTE, 0 );
+        calendar.set( Calendar.SECOND, 0 );
+        calendar.set( Calendar.MILLISECOND, 0 );
+        return calendar.getTime();
+    }
+
+    /**
+     * 时间增加N分钟
+     */
+    public static Date addDateMinutes(Date date, int minutes) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime( date );
+        calendar.add( Calendar.MINUTE, minutes );// 24小时制
+        date = calendar.getTime();
+        return date;
+    }
+
+    /**
+     * 时间递增N小时
+     */
+    public static Date addDateHour(Date date, int hour) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime( date );
+        calendar.add( Calendar.HOUR, hour );// 24小时制
+        date = calendar.getTime();
+        return date;
+    }
+
+    /**
+     * 获取时间菜单
+     */
+    public static List<Date> getDateMenus() {
+
+        //定义一个List<Date>集合，存储所有时间段
+        List<Date> dates = new ArrayList<Date>();
+
+        //循环12次
+        Date date = toDayStartHour( new Date() ); //凌晨
+        for (int i = 0; i < 12; i++) {
+            //每次递增2小时,将每次递增的时间存入到List<Date>集合中
+            dates.add( addDateHour( date, i * 2 ) );
+        }
+
+        //判断当前时间属于哪个时间范围
+        Date now = new Date();
+        for (Date cdate : dates) {
+            //开始时间<=当前时间<开始时间+2小时
+            if (cdate.getTime() <= now.getTime() && now.getTime() < addDateHour( cdate, 2 ).getTime()) {
+                now = cdate;
+                break;
+            }
+        }
+
+        //当前需要显示的时间菜单
+        List<Date> dateMenus = new ArrayList<Date>();
+
+        for (int i = 0; i < 5; i++) {
+            dateMenus.add( addDateHour( now, i * 2 ) );
+        }
+        return dateMenus;
+    }
+
+    /**
+     * 时间转成yyyyMMddHH
+     */
+    public static String date2Str(Date date) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat( "yyyyMMddHH" );
+        return simpleDateFormat.format( date );
+    }
+}
---
+++ changgou_service/changgou_service_seckill/src/main/java/com/changgou/seckill/task/SecKillGoodsPushTask.java
@@ -0,0 +1,80 @@
+package com.changgou.seckill.task;
+
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.ObjectUtil;
+import com.changgou.common.util.DateUtil;
+import com.changgou.seckill.dao.SeckillGoodsMapper;
+import com.changgou.seckill.pojo.SeckillGoods;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @Author: Haotian
+ * @Date: 2020/3/5 18:06
+ * @Description: 秒杀定时任务
+ */
+@Component
+public class SecKillGoodsPushTask {
+    /**
+     * redis 中秒杀商品 key 前缀
+     */
+    private static final String SEC_KILL_GOODS_KEY = "sec_kill_goods_key";
+    @Autowired
+    private SeckillGoodsMapper seckillGoodsMapper;
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Scheduled(cron = "0/30 * * * * ?")
+    public void loadSecKillGoodsToRedis() {
+        //查询所有符合条件的秒杀商品
+        //1.获取时间段集合并循环遍历出每一个时间段
+        List<Date> dateMenus = DateUtil.getDateMenus();
+        for (Date dateMenu : dateMenus) {
+            //2.获取每一个时间段名称,用于后续redis中key的设置
+            String redisExtName = DateUtil.date2Str( dateMenu );
+            //3.拼装查询条件
+            Example example = getExample( dateMenu, redisExtName );
+            //4.执行查询获取对应的结果集
+            List<SeckillGoods> secKillGoodsList = seckillGoodsMapper.selectByExample( example );
+            //5.将秒杀商品存入缓存
+            for (SeckillGoods goods : secKillGoodsList) {
+                redisTemplate.opsForHash().put( SEC_KILL_GOODS_KEY + redisExtName, goods.getId(), goods );
+            }
+        }
+    }
+
+    /**
+     * 构建查询对象
+     *
+     * @param dateMenu     当前时间段
+     * @param redisExtName 当前时间段转为yyyyMMddHH的值
+     * @return 查询条件对象
+     */
+    private Example getExample(Date dateMenu, String redisExtName) {
+        Example example = new Example( SeckillGoods.class );
+        Example.Criteria criteria = example.createCriteria();
+        //状态必须为审核通过 status=1
+        criteria.andEqualTo( "status", "1" );
+        //商品库存个数>0
+        criteria.andGreaterThan( "stockCount", 0 );
+        //秒杀商品开始时间>=当前时间段
+        String startTime = cn.hutool.core.date.DateUtil.formatDateTime( dateMenu );
+        criteria.andGreaterThanOrEqualTo( "startTime", startTime );
+        //秒杀商品结束<当前时间段+2小时
+        String endTime = cn.hutool.core.date.DateUtil.formatDateTime( cn.hutool.core.date.DateUtil.offsetHour( dateMenu, 2 ) );
+        criteria.andLessThan( "endTime", endTime );
+        //排除之前已经加载到Redis缓存中的商品数据 key field=对象id value=对象
+        Set field = redisTemplate.boundHashOps( SEC_KILL_GOODS_KEY + redisExtName ).keys();
+        if (CollUtil.isNotEmpty( field )) {
+            criteria.andNotIn( "id", field );
+        }
+        return example;
+    }
+}
